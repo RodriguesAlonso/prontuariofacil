b7cc6d082e4c680f14812d03020deba3aef78410
Alonso <joaoalonso42@gmail.com>
2022-05-26 21:36:50 -0300

    fix site

--- prontuariofacil/ext/site/site.py
+++ prontuariofacil/ext/site/site.py
@@ -3,32 +3,43 @@ from prontuariofacil.ext.models.form import Form_pront, Form_medico
 from prontuariofacil.ext.models.models import submit_pront, submit_medico
 from prontuariofacil.ext.models.tables import Prontuario, Medico
 
-site = Blueprint('site', __name__, static_folder='static',  template_folder='templates', static_url_path='/static/site')
+site = Blueprint(
+    "site",
+    __name__,
+    static_folder="static",
+    template_folder="templates",
+    static_url_path="/static/site",
+)
 
-@site.route('/')
+
+@site.route("/")
 def index():
     return render_template("index.html")
+
 
 @site.route("/login")
 def login():
     return render_template("login.html")
 
-@site.route('/recuperar-senha')
-def recupera_senha():
-    return render_template('recuperar_senha.html')
 
-@site.route('/cadastro-prontuario', methods=["GET", "POST"])
+@site.route("/recuperar-senha")
+def recupera_senha():
+    return render_template("recuperar_senha.html")
+
+
+@site.route("/cadastro-prontuario", methods=["GET", "POST"])
 def cadastro_pront():
     name = None
     form = Form_pront()
-    prontuario = Prontuario()    
+    prontuario = Prontuario()
     submit_pront(prontuario, form)
-    return render_template("cadastro_prontuario.html", prontuario=form, name = name)
+    return render_template("cadastro_prontuario.html", prontuario=form, name=name)
+
 
 @site.route("/cadastro-medico", methods=["GET", "POST"])
 def cadastro_medic():
-    medico = Medico()
     name = None
     form = Form_medico()
+    medico = Medico()
     submit_medico(medico, form)
-    return render_template("cadastro_medico.html",form = form, name = name, medico=medico)
+    return render_template("cadastro_medico.html", medico=form, name=name)
